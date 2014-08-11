ef4b1937c6352e16b7d5e3defccf4a24f24f91b5
Kentaro Kawano <kentaro.kawano@gmail.com>
2014-08-11 12:59:41 +0900

    ansible のsecure_pathとenv_keepを変更した

--- useradd-ansible.sh
+++ useradd-ansible.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 useradd ansible
-echo "ansible ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/ansible
+cat <<SUDOER > /etc/sudoers.d/ansible
+Defaults:ansible    secure_path = /sbin:/bin:/usr/sbin:/usr/local/bin:/usr/bin
+Defaults:ansible    env_keep += "PATH"
+ansible ALL=(ALL) NOPASSWD:ALL
+SUDOER
+
 sudo -u ansible mkdir -p -m 700 /home/ansible/.ssh
 sudo -u ansible curl -o /home/ansible/.ssh/authorized_keys https://github.com/kawaken.keys
 chmod 600 /home/ansible/.ssh/authorized_keys
